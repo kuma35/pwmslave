83aaaa1638bf20b43152edeb95d21041bd2af424
kuma35 <sindobook@gmail.com>
2016-03-12 10:48:46 +0900

    add SoftwareSerial

--- src/PwmSlave.ino
+++ src/PwmSlave.ino
@@ -1,156 +1,273 @@
 // PwmSlave.ino
+// packet version.
+
+// short packet
+// 'EF','65', <<Flag>>, <<ID>>, <<addr>>, <<len>>, <<count>>, <<data>>..., <<<sum>>
+// Header:EF65 :-)
+// Flag:
+// ID: 0
+// Addr: below
+// len: below
+// count: 1
+// data: below
+// sum:
+//
+// return packet
+// 'EF', '66', <<flag>> 
+
+// virtual register
+// 0-10 copy from EEPROM at setup(). FLAG_FLASH is 1 then write to EEPROM.
+// 11-  register. attach and pulse.
+// addr description                 default
+// ==== =========================== =======
+//    0 I2C Address                       0
+//    1 Servo 0 digital port number       5
+//    2 Servo 1 digital port number       6
+//    3 Servo 0 minimum pulse (L,H)     800
+//    4 -                                 -
+//    5 Servo 0 maximam pulse (L,H)    2200
+//    6 -                                 -
+//    7 Servo 1 minimum pulse (L,H)     800
+//    8 -                                 -
+//    9 Servo 1 maximam pulse (L,H)    2200
+//   10 -                                 -
+//   11 Servo 0 attach flag               0
+//   12 Servo 1 attach flag               0
+//   13 Servo 0 pulse (L,H)            1100
+//   14 -                                 -
+//   15 Servo 1 pulse (L,H)            1100
+//   16 -                                 -
+
 
 #include <EEPROM.h>
 #include <HardwareSerial.h>
 #include <Servo.h>
-#include <string.h>
+#include <SoftwareSerial.h>
 
-#include "TinyShell.h"
+#define DEBUG
 
+#define MEMORY_SIZE 32
+#define RECV_SIZE 32
+#define RESP_SIZE 32
 #define SERVO_VOLUME 2
-#define CR '\x0d'
-#define LF '\x0a'
-#define ERR_STACK_OVERFLOW "Stack over flow"
-#define EEPROM_I2C_ADDR	0
+
+#define FLAG_ACK 0x01
+#define FLAG_FLASH 0x40
+
+#define EEPROM_BASE 0
+#define EEPROM_I2C_ADDR	0	// NO USE
 #define EEPROM_SERVO_0 1 
 #define EEPROM_SERVO_1 2
-#define RECEIVE_BUF_LEN 130
 #define EEPROM_SERVO_0_MIN 3	// big endian 2 byte int. 0x1234 to 0x12,0x34
 #define EEPROM_SERVO_0_MAX 5	// big endian 2 byte int. 0x1234 to 0x12,0x34
 #define EEPROM_SERVO_1_MIN 7	// big endian 2 byte int. 0x1234 to 0x12,0x34
 #define EEPROM_SERVO_1_MAX 9	// big endian 2 byte int. 0x1234 to 0x12,0x34
+#define EEPROM_LAST 11
+#define RAM_ATTACH_0 11
+#define RAM_ATTACH_1 12
+#define RAM_PULSE_0  13
+#define RAM_PULSE_1  15
 
-Servo Servos[SERVO_VOLUME];
-int Servo_min[SERVO_VOLUME];
-int Servo_max[SERVO_VOLUME];
-int Servo_pin[SERVO_VOLUME];
+#ifdef DEBUG
+SoftwareSerial MySerial(2, 3);
+#endif
+Servo Sv[SERVO_VOLUME];
 
-class PwmSlaveShell : public TinyShell {
- public:
-  int execute(String *tokenp);
-};
+unsigned char Memory[MEMORY_SIZE];
+unsigned char Recv[RECV_SIZE];
+unsigned char Resp[RESP_SIZE];
 
-int PwmSlaveShell::execute(String *tokenp) {
-  int status = 100;
-  if (tokenp->equals("pulse")) {	// ( <<servo_no>>,<<microseonds>> --)	local rc servo writeMicroseconds
-    if (this->_data_stack.popable(2)) {
-      int value = this->_data_stack.pop();
-      int servo_no = this->_data_stack.pop();
-      if ( 0 <= servo_no and servo_no < SERVO_VOLUME) {
-	Servos[servo_no].writeMicroseconds(value);
-	status += 1;
-      } else {
-	this->_serial->println(F("Out of servo no."));
-      }
-    } else {
-      this->_serial->print(tokenp->c_str());
-      this->_serial->println(" Stack underflow.");
-    }
-  } else if (tokenp->equals("angle")) {	// ( <<servo_no>>,<<degbree>> --)	local rc servo write. 0 to 180.
-    if (this->_data_stack.popable(2)) {
-      int value = this->_data_stack.pop();
-      int servo_no = this->_data_stack.pop();
-      if ( 0 <= servo_no and servo_no < SERVO_VOLUME) {
-	Servos[servo_no].write(value);
-	status += 1;
-      } else {
-	this->_serial->println(F("Out of servo no."));
-      }
-    } else {
-      this->_serial->print(tokenp->c_str());
-      this->_serial->println(" Stack underflow.");
-    }
-  } else if (tokenp->equals("attach")) {	// <<servo_no>> -- attach to Digital pin to servo.
-    if (this->_data_stack.popable(1)) {
-      int servo_no = this->_data_stack.pop();
-      if ( 0 <= servo_no and servo_no < SERVO_VOLUME) {
-	Servos[servo_no].attach(Servo_pin[servo_no], Servo_min[servo_no], Servo_max[servo_no]);
-	status += 1;
-      } else {
-	this->_serial->println(F("Out of servo no."));
-      }
-    } else {
-      this->_serial->print(tokenp->c_str());
-      this->_serial->println(" Stack underflow.");
-    }
-  } else if (tokenp->equals("detach")) {	// <<servo_no>> -- detach from Digital pin to servo.
-    if (this->_data_stack.popable(1)) {
-      int servo_no = this->_data_stack.pop();
-      if ( 0 <= servo_no and servo_no < SERVO_VOLUME) {
-	Servos[servo_no].detach();
-	status += 1;
-      } else {
-	this->_serial->println(F("Out of servo no."));
-      }
-    } else {
-      this->_serial->print(tokenp->c_str());
-      this->_serial->println(" Stack underflow.");
-    }
-  } else {
-    status = TinyShell::execute(tokenp);
-  }
-  return status;
+
+int Recv_index = 0;
+
+int get_servo_pin(int id) {
+  switch (id) {
+  case 0:
+    return Memory[EEPROM_SERVO_0];
+  case 1:
+    return Memory[EEPROM_SERVO_1];
+  } 
 }
 
-PwmSlaveShell Shell;
+int get_servo_min(int id) {
+  switch (id) {
+  case 0:
+    return Memory[EEPROM_SERVO_0_MIN] << 8 |
+      Memory[EEPROM_SERVO_0_MIN+1];
+  case 1:
+    return Memory[EEPROM_SERVO_1_MIN] << 8 |
+      Memory[EEPROM_SERVO_1_MIN+1];
+  }
+}
+
+int get_servo_max(int id) {
+  switch (id) {
+  case 0:
+    return Memory[EEPROM_SERVO_0_MAX] << 8 |
+      Memory[EEPROM_SERVO_0_MAX+1];
+  case 1:
+    return Memory[EEPROM_SERVO_1_MAX] << 8 |
+      Memory[EEPROM_SERVO_1_MAX+1];
+  }
+}
+
+int get_attach(int id) {
+  switch (id) {
+  case 0:
+    return Memory[RAM_ATTACH_0];
+  case 1:
+    return Memory[RAM_ATTACH_1];
+  }
+}
+
+int get_pulse(int id) {
+  switch (id) {
+  case 0:
+    return Memory[RAM_PULSE_0] << 8 |
+      Memory[RAM_PULSE_0+1];
+  case 1:
+    return Memory[RAM_PULSE_1] << 8 |
+      Memory[RAM_PULSE_1+1];
+  }
+}
+
+int get_checksum(unsigned char *datap, int len) {
+  int sum = datap[0];
+  for (int i = 1; i < len; i++) {
+    sum ^= datap[i];
+  }
+  return sum;
+}
+
+int recv_to_memory(unsigned char *recvp, int len, int addr, unsigned char memory[]) {
+  for (int i = 0; i < len; i++) {
+    memory[addr+i] = recvp[i];
+  }
+}
+
+
+
+void drop_packet(void) {
+  while (Serial.available() > 0) {
+    delay(10);
+    Serial.read();
+  }
+}
+
+int data_recv(int bytes, unsigned char *datap) {
+  for (int index = 0; index < bytes ; index++ ) {
+    if (Serial.peek() != -1) {
+      datap[index] = Serial.read();
+    } else {
+      return 0;
+    }
+  }
+  return 1;
+}
+
+#ifdef DEBUG
+void memory_dump(void) {
+  for (int i = 0; i< MEMORY_SIZE; i++) {
+    MySerial.print(Memory[i], HEX);
+    //print_hex(Memory[i]);
+    MySerial.print(F(","));
+  }
+  MySerial.println(F(""));
+}
+#endif
 
 void setup()
 {
+#ifdef DEBUG
+  MySerial.begin(9600);
+  MySerial.println("NOW DEBUGGING");
+#endif
   Serial.begin(9600);
-  Servo_pin[0] = EEPROM.read(EEPROM_SERVO_0);
-  Serial.print(F("Servo 0 to D"));
-  Serial.print(Servo_pin[0]);
-  //Servos[0].attach(servo_pin);
-  Servo_min[0] = ((int)EEPROM.read(EEPROM_SERVO_0_MIN) << 8) |
-    (EEPROM.read(EEPROM_SERVO_0_MIN+1));
-  Servo_max[0] = ((int)EEPROM.read(EEPROM_SERVO_0_MAX) << 8) |
-    (EEPROM.read(EEPROM_SERVO_0_MAX+1));
-  Serial.print(F(" "));
-  Serial.print(Servo_min[0]);
-  Serial.print(F(" - "));
-  Serial.println(Servo_max[0]);
-  
-  Servo_pin[1] = EEPROM.read(EEPROM_SERVO_1);
-  Serial.print(F("Servo 1 to D"));
-  Serial.print(Servo_pin[1]);
-  //Servos[1].attach(servo_pin);
-  Servo_min[1] = ((int)EEPROM.read(EEPROM_SERVO_1_MIN) << 8) |
-    (EEPROM.read(EEPROM_SERVO_1_MIN+1));
-  Servo_max[1] = ((int)EEPROM.read(EEPROM_SERVO_1_MAX) << 8) |
-    (EEPROM.read(EEPROM_SERVO_1_MAX+1));
-  Serial.print(F(" "));
-  Serial.print(Servo_min[1]);
-  Serial.print(F(" - "));
-  Serial.println(Servo_max[1]);
-
-  Serial.print(">");
+  // ROM to RAM
+  for(int i = EEPROM_BASE; i < EEPROM_LAST; i++) {
+    Memory[i] = EEPROM.read(i);
+  }
+#ifdef DEBUG
+  memory_dump();
+#endif
 }
-
-String buffer;
-String token;
 
 void loop()
 {
-  if (Serial.available() >0) {
+  int data = -1;
+  if (Serial.available() > 0) {
     delay(10);
-    int last_char = Shell.get_line();
-    if (last_char == CR) {
-      while (Shell.get_token(&token)) {
-	if (Shell.get_number(&token)) {
-	} else if (Shell.execute(&token) <= 100) {
-	  // command execution status:
-	  // 0;unkown word
-	  // 100<=status;command execute done. and error
-	  // 101>=status;command execute done. and success continue next word execute...
-	  Serial.print(F("stop. not yet exec words: "));
-	  Serial.println(Shell.buffer().c_str());
-	  Shell.clear_buffer();
-	  break;	// stop interpreting
+    Recv_index = 0;
+    if (!data_recv(7, Recv+Recv_index)) {
+      drop_packet();
+      return;
+    }
+#ifdef DEBUG
+    MySerial.print(F("read header:"));
+    memory_dump();
+#endif
+    if (Recv[0] != 0xEF || Recv[1] != 0x65) {
+      drop_packet();
+      return;
+    }
+    if (Recv[2] & FLAG_ACK) {
+      drop_packet();
+      Serial.write(0x07);
+      return;
+    }
+    if (Recv[2] & FLAG_FLASH) {
+      drop_packet();
+      for (int adr; adr < EEPROM_LAST; adr++) {
+	if (EEPROM.read(adr) != Memory[adr]) {
+	  EEPROM.write(adr, Memory[adr]);
 	}
       }
-      Serial.print(F(">"));
-    } else {
-      // nothing
+    }
+    Recv_index += 7;
+    if (!data_recv(Recv[5], Recv+Recv_index)) {
+      drop_packet();
+      return;
+    }
+    recv_to_memory(Recv+7, Recv[4], Recv[5], Memory);
+    Recv_index += Recv[5];
+    if (!data_recv(1, Recv+Recv_index)) {
+      drop_packet();
+      return;
+    }
+#ifdef DEBUG
+    MySerial.print(F("Recv_index:"));
+    MySerial.println(Recv_index);
+    MySerial.print(F("recv packet:"));
+    memory_dump();
+    MySerial.print(F("get_checksum"));
+    MySerial.println(get_checksum(Recv+3, Recv_index - 1));
+#endif
+    if (Recv[Recv_index] != get_checksum(Recv+3, Recv_index - 1)) {
+      drop_packet();
+      return;
     }
   }
- }
+  // memory check and run
+  for (int servo_id = 0; servo_id < 2; servo_id++) {
+    int pin = get_attach(servo_id);
+    if (pin != 0) {
+      if (!Sv[servo_id].attached()) {
+	Sv[servo_id].attach(pin);
+      }
+      // writeMicrosecond only attached.
+      int pulse = get_pulse(servo_id);
+      int min = get_servo_min(servo_id);
+      int max = get_servo_max(servo_id);
+      if (pulse < min) {
+	  pulse = min;
+      } else if ( pulse > max) {
+	pulse = max;
+      }
+      Sv[servo_id].writeMicroseconds(pulse);
+    } else {
+      if (Sv[servo_id].attached()) {
+	Sv[servo_id].detach();
+      }
+    }
+  }
+}
